6cc3192b25022d1b66992da8fa4664601624ace4
Pujon Das Auvi <auvipujondas@gmail.com>
2025-12-21 11:56:22 +0600

    feat: add Docker support with Dockerfile and docker-compose, update app service and .gitignore

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('Application is starting...');
   await app.listen(process.env.PORT ?? 3000);
+  // eslint-disable-next-line prettier/prettier
+  console.log(
+    `Application is running on: ${await app.getUrl()}`,
+  );
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Error during bootstrap:', err);
+  process.exit(1);
+});
