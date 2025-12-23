db9e1f2aea87f7660407c46801843a7fe4763e33
Pujon Das Auvi <auvipujondas@gmail.com>
2025-12-23 18:30:16 +0600

    feat: introduce `responseBlogDto` and add unit tests for `BlogService` methods.

--- src/blog/blog.service.spec.ts
+++ src/blog/blog.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BlogService } from './blog.service';
+import { createBlogDto } from './dto/createBlog.dto';
+import { responseBlogDto } from './dto/responseBlog.dto';
 
 describe('BlogService', () => {
   let service: BlogService;
@@ -14,5 +16,49 @@ describe('BlogService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+
+  describe('createPost', () => {
+    it('should create a post', () => {
+      const payload: createBlogDto = {
+        title: 'Test Post',
+        content: 'Test Content',
+        published: true,
+        authorId: 1,
+        author: 'Test Author',
+      };
+      const result: responseBlogDto = {
+        ...payload,
+        id: 1,
+        title: payload.title ?? undefined,
+        content: payload.content ?? undefined,
+        published: payload.published ?? undefined,
+        authorId: payload.authorId ?? undefined,
+        author: payload.author ?? undefined,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+      expect(service.createPost(payload)).resolves.toEqual(result);
+    });
+  });
+
+
+  describe('getAllPost', () => {
+    it('should return all posts', () => {
+      const result: responseBlogDto[] = [
+        {
+          id: 1,
+          title: 'Test Post',
+          content: 'Test Content',
+          published: true,
+          authorId: 1,
+          author: 'Test Author',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ];
+      expect(service.getAllPost()).resolves.toEqual(result);
+    });
   });
 });
